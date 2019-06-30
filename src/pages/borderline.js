72a5f740a1ed1fdfed35e7b7bb71e4046e51f36a
hanyang.tay <hanyang.tay@go-jek.com>
2019-06-30 00:01:30 -0700

    fix music direct borderline

--- src/pages/borderline.js
+++ src/pages/borderline.js
@@ -35,6 +35,7 @@ const BorderlinePage = ({ data }) => (
         <i> Book: Aryanna Garber </i> <br />
         <i> Music & Lyrics: Benjamin Velez </i> <br />
         <i> Directed by Jaki Bradley </i> <br />
+        <i> Music Directed by David Gardos </i> <br />
         <i> National Music Theater Conference </i> <br /> <br /> 
 
         <p>After being expelled from college, Anna is stuck with her soon-to-be-remarried father and a revolving door of therapists — thanks to court-mandated probation. This, coupled with a new diagnosis of Borderline Personality Disorder, forces Anna to grapple with demons she has always lived with but never had a name for, until now. Through a unique fusion of pop melodies, jazz harmonies, funk beats and hypnotic waltzes, the score captures the chaotic beauty of a psyche. As this darkly comedic musical asks, how do you reach out for help when you’re afraid to admit you’re sick? </p>
